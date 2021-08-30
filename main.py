60962fd21a8f3360b3d35bf53d118aaa3db7e2ce
Anton Rahmn <89807437+feddo8@users.noreply.github.com>
2021-08-30 19:48:04 +0000

    bytte n**0.5 till sqrt funktionen fråm math

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
 import datetime
 now = datetime.datetime.now()
 print (now.strftime("%Y-%m-%d %H:%M:%S"))
-   
+from math import sqrt
 primtal = [2,3]
 n=3
 while True:
   n+=2
   delbar = False
-  sqrt = n**(0.5)
+  sqrtn = sqrt(n)
   for nummer in primtal:
     if n % nummer == 0:
       delbar = True
-    if nummer > sqrt:
+    if nummer > sqrtn:
       break
   
   if delbar == False:
     primtal.append(n)
-  if n > 1000000:
+  if n > 2500000:
     break
-
+now1 = datetime.datetime.now()
 for nummer in primtal:
   print(nummer)
 
-now1 = datetime.datetime.now()
 print (now.strftime("%Y-%m-%d %H:%M:%S"))
 print (now1.strftime("%Y-%m-%d %H:%M:%S"))
 
